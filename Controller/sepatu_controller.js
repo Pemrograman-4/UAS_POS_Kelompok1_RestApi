9363e80af4c14c38c9a0e88d15bdfc3337604702
sandyc02 <sandychandra21@gmail.com>
2021-12-02 22:16:40 +0700

    delete dan update

--- Controller/sepatu_controller.js
+++ Controller/sepatu_controller.js
@@ -25,5 +25,35 @@ exports.add = (data) =>
       .then(() =>
         resolve(response.suksesResponse("Berhasil Menambah Data Sepatu"))
       )
-      .catch(() => reject(response.errorResponse("Gagal Menambah Data Sepatu")));
+      .catch(() =>
+        reject(response.errorResponse("Gagal Menambah Data Sepatu"))
+      );
   });
+
+    // Delete data
+exports.delete = async (req, res) => {
+try {
+await Model.findOneAndDelete({ _id: req.params.id });
+res.json(response.suksesResponse("Berhasil Menghapus Data"));
+} catch (error) {
+res.json(response.errorResponse("Gagal Menghapus Data"));
+}
+};
+
+// update data
+exports.update = async (req, res) => {
+  try {
+    await Model.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        warna: req.body.warna,
+        deskripsi: req.body.deskripsi,
+        merk: req.body.merk,
+        size: req.body.size,
+      }
+    );
+    res.json(response.suksesResponse("Berhasil Update Data"));
+  } catch (error) {
+    res.json(response.errorResponse("Gagal Update Data"));
+  }
+};
--- Router/sepatu_router.js
+++ Router/sepatu_router.js
@@ -6,10 +6,17 @@ router.post("/add", (req, res) => {
     .then((result) => res.json(result))
     .catch((err) => res.json(err));
 });
+//get data
 router.get("/get", (req, res) => {
   Controller.get(req.body)
     .then((result) => res.json(result))
     .catch((err) => res.json(err));
 });
+
+// delete data
+router.delete("/delete/:id", Controller.delete);
+
+// update data
+router.put("/update/:id", Controller.update);
 
 module.exports = router;
